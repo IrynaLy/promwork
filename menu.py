19e1d84ec57978afcd3fa4c673a7325ab65431eb
Iryna Lysenko <60301049+IrynaLy@users.noreply.github.com>
2020-03-01 22:46:43 +0200

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,31 @@
+import sys
+
+def create():
+    print("enter name")
+    pass
+
+def find():
+    print("enter name or number")
+    pass
+
+def edit():
+    print("make a change")
+    pass
+
+def delete():
+    print("are you sure?")
+    pass
+
+def close():
+    print("please wait")
+sys.exit(0)
+
+def show_menu():
+    print("Menu")
+    print("Create contact")
+    print("Find contact")
+    print("Edit contact")
+    print("Delete contact")
+    print("Exit")
+    on_item_selected()
+
